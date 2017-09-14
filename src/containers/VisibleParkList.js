6acf27faa7ef328f62cb3e945cb4a504b9168de8
Alice Huang <alice@expertise.com>
2017-09-14 10:23:11 -0700

    Make container work

--- src/containers/VisibleParkList.js
+++ src/containers/VisibleParkList.js
@@ -2,18 +2,16 @@ import { connect } from 'react-redux';
 import { toggleFeature } from '../actions/actions';
 import ParkList from '../components/ParkList';
 
-const getVisibleParks = (parks, filter) => {
-  switch (filter) {
-    case 'SHOW_FEATURED':
-      return parks.filter(park => park.featured);
-    default:
-      return parks;
+const getVisibleParks = (parks, filters) => {
+  if (filters.length > 0) {
+    return parks.filter(park => park.featured);
   }
+  return parks;
 };
 
 const mapStateToProps = state => {
   return {
-    parks: getVisibleParks(state.parks)
+    parks: getVisibleParks(state.parks, state.shownFeatures)
   }
 };
 
