159052a448ef008d8aa2ec66dec488c2a7862ca7
Alice Huang <alice@expertise.com>
2017-09-14 09:53:01 -0700

    Return multiple features in one park

--- src/components/Park.js
+++ src/components/Park.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Park = ({ name, features, hours, location }) => (
   <li>
     <h4>{name}</h4>
-    <div>{features}</div>
+    {features.map((feature, i) => <div key={i}>{feature}</div>)}
     <div>{hours}</div>
     <div>{location}</div>
   </li>
