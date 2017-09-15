852474752a9c601cb8d4fdb2b5d1655dd3c86c28
Alice Huang <alice@expertise.com>
2017-09-15 11:31:17 -0700

    Add renamed folder/file

---
+++ database/Park.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const ParkSchema = new mongoose.Schema({
+  id: { type : String, required : true, unique: true, dropDups : true },
+  name: { type: String, required: true },
+  features: {type: Array, required: true },
+  hours: String,
+  location: Object
+});
+
+mongoose.model('Park', ParkSchema);
+
+module.exports = mongoose.model('Park');
